907e732d76f2badb29e30313eba9dc05b68e2a52
erhunt29 <titov.av23@gmail.com>
2018-12-13 18:45:52 +0300

    last commit

---
+++ precache-manifest.5a906a30f2718037c14e49974fa97403.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d4b1f4f8267306a0804e4830ce46a90",
+    "url": "./static/media/dress-2.2d4b1f4f.png"
+  },
+  {
+    "revision": "ce59b87f5f96ec7f4c96",
+    "url": "./static/css/main.9409892b.chunk.css"
+  },
+  {
+    "revision": "ba1e532b62ed4c5ac61d",
+    "url": "./static/js/1.ba1e532b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "899eb0161c68b7bea1e404b6831e32bf",
+    "url": "./static/media/sunglusses.899eb016.png"
+  },
+  {
+    "revision": "83ce099260479afc4dcae18123b055d4",
+    "url": "./static/media/hat.83ce0992.png"
+  },
+  {
+    "revision": "93c7b869afa62ba7f8b73e2a3df4f21a",
+    "url": "./static/media/dress-1.93c7b869.png"
+  },
+  {
+    "revision": "ce59b87f5f96ec7f4c96",
+    "url": "./static/js/main.ce59b87f.chunk.js"
+  },
+  {
+    "revision": "9cebcff8cb5ba2b1598d7df944b7d1f7",
+    "url": "./static/media/good_item_1.9cebcff8.png"
+  },
+  {
+    "revision": "2592f6a337e63c08c6767473bf7518b1",
+    "url": "./static/media/good_item_2.2592f6a3.png"
+  },
+  {
+    "revision": "541a8e33ce07bec9955efd5019aba255",
+    "url": "./static/media/good_item_3.541a8e33.png"
+  },
+  {
+    "revision": "541a8e33ce07bec9955efd5019aba255",
+    "url": "./static/media/good_item_4.541a8e33.png"
+  },
+  {
+    "revision": "dc7703a2886264349e7fddb8ced26e7c",
+    "url": "./static/media/Dear Joe.dc7703a2.ttf"
+  },
+  {
+    "revision": "733cc2abdd356f591aca6d1986273352",
+    "url": "./index.html"
+  }
+];
